da8e0b3ac41af812e0791223b8033f5fb0691dba
admin <admin@admin-PC>
2016-04-06 12:54:13 +0530

    Edge detection and shape extraction using standard deviation filter. Work to be done on window size

---
+++ Cell_Edges.m
@@ -0,0 +1,32 @@
+figure,
+for i = 6
+    if i < 10
+        fname = ['40x-dic-egfp-pc12-ngfxy0',num2str(i),'c2.tif'];
+    end
+    if i >= 10
+        fname = ['40x-dic-egfp-pc12-ngfxy',num2str(i),'c2.tif'];
+    end
+    info = imfinfo(fname);
+    number_of_images = numel(info);
+    %Read TIF Stack
+    for k = 1:number_of_images
+        im = imread(fname,k);
+        im = mat2gray(im);
+        im = medfilt2(im,[5,5]);
+        % edge detection
+        thresh = graythresh(im);
+        bw = im2bw(im,thresh);
+        rprop = regionprops(bw,'Area','MajorAxisLength','MinorAxisLength','Perimeter');
+        majax(k) = rprop.MajorAxisLength;
+        minax(k) = rprop.MinorAxisLength;
+        area(k) = rprop.Area;
+        perim(k) = rprop.Perimeter;
+        edg = bwperim(bw);
+        imrgb = repmat(im,[1,1,3]);
+        [i,j] = find(edg == 1);
+        for l = 1:length(i)
+            imrgb(i(l),j(l),1) = 1;
+        end
+        subplot(4,2,k), imshow(imrgb);
+    end
+end
