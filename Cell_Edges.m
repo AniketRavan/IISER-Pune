b5ec647481bd3812d45afbc61aaa2fc3f65f70bf
admin <admin@admin-PC>
2016-04-07 14:51:33 +0530

    Supervised cropping

--- Cell_Edges.m
+++ Cell_Edges.m
@@ -11,9 +11,13 @@ for i = 6
     end
     info = imfinfo(fname);
     number_of_images = numel(info);
+    imfull = imread(fname,1);
+    imshow(mat2gray(imfull));
+    [x,y] = ginput(1);
     %Read TIF Stack
     for k = 1:number_of_images
-        im = imread(fname,k);
+        imfull = imread(fname,k);
+        im = imfull(y-120:y+120,x-120:x+120);  % Cropping ROI containing cell
         im = mat2gray(im);
         im = medfilt2(im,[5,5]);
         % Edge detection using standard deviation filter
